8660a708d0cfac73bde9239b6eaba55ac4256c21
ountzza <pond1982a@gmail.com>
2015-04-25 11:46:35 +0700

    to string format

--- src/contact/say.go
+++ src/contact/say.go
@@ -1,5 +1,34 @@
 package contact
 
+// Form user defined type
+type Form struct {
+	Sender
+	Content string
+}
+
+// Sender user defined type
+type Sender struct {
+	Firstname string
+	Lastname  string
+}
+
+// toString format string
+func (f Form) toString() string {
+	return "#" + f.Firstname + "*" + f.Lastname + "$" + f.Content
+}
+
+// NewForm return form object
+func NewForm(firstname string, lastname string, msg string) *Form {
+	return &Form{
+		Sender: Sender{
+			Firstname: firstname,
+			Lastname:  lastname,
+		},
+		Content: msg,
+	}
+}
+
+// Say return Hi
 func Say() string {
 	return "Hi"
 }
