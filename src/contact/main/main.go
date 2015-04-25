0a51ba6f81db1d4e13eb1345b54f07851b7c1e9a
ountzza <pond1982a@gmail.com>
2015-04-25 11:38:52 +0700

    another way to send/recieved objects between two classes

--- src/contact/main/main.go
+++ src/contact/main/main.go
@@ -8,4 +8,16 @@ import (
 func main() {
 	msg := contact.Say()
 	fmt.Println(msg)
+
+	/*
+		contact := contact.Form{
+			Sender: contact.Sender{
+				Firstname: "Pondd",
+				Lastname:  "NoobMe",
+			},
+			Content: "this is a message",
+		}
+	*/
+	form := contact.NewForm("Pondd", "NoobMe", "This is a message")
+	fmt.Println(form)
 }
